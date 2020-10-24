42baddb39363ccf237418395c2142a9d030339c0
kimDbicon <kimryd@naver.com>
2020-10-24 10:36:13 +0900

    complete!!

---
+++ src/main/java/com/pmis/mni/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.pmis.mni.springboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
